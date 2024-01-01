3d8c24b2b6cb94be0b0302fef4174244dc157c51
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-01 13:11:48 +0100

    chore: add nav to mainnav

--- components/MainNav.jsx
+++ components/MainNav.jsx
@@ -5,10 +5,21 @@ import LogoImage from "@/assets/logo.png";
 function MainNav() {
   return (
     <header>
-      <Link>
+      <Link href="/">
         <img src={LogoImage.src} alt="A plate with lots of food init" />
         NextLevel Foods
       </Link>
+
+      <nav>
+        <ul>
+          <li>
+            <Link href="/meals"> Community meals </Link>
+          </li>
+          <li>
+            <Link href="/meals/community"> Foodie community </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
