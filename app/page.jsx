3eaaf6651c95179fc0935522f12917bec6c90655
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-01 22:29:15 +0100

    chore:  update page comp

--- app/page.jsx
+++ app/page.jsx
@@ -8,14 +8,13 @@ export default function Home() {
       <header className={classes.header}>
         <div className={classes.slideshow}></div>
 
-        <div className="">
+        <div className={classes.hero}>
           <h1> NextLevel Foods for NextLevel Foodies </h1>
           <p>Tast and share food from all over the world</p>
-
-          <div className={classes.cta}>
-            <Link href="/community"> Join the community </Link>
-            <Link href="meals"> Explore Meals </Link>
-          </div>
+        </div>
+        <div className={classes.cta}>
+          <Link href="/community"> Join the community </Link>
+          <Link href="meals"> Explore Meals </Link>
         </div>
       </header>
     </>
