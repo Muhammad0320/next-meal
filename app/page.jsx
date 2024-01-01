24aa793316e5ec6be750e95969a1872790621254
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-01 22:28:08 +0100

    chore:  update page comp

---
+++ app/page.jsx
@@ -0,0 +1,23 @@
+import classes from "./page.module.css";
+
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.slideshow}></div>
+
+        <div className="">
+          <h1> NextLevel Foods for NextLevel Foodies </h1>
+          <p>Tast and share food from all over the world</p>
+
+          <div className={classes.cta}>
+            <Link href="/community"> Join the community </Link>
+            <Link href="meals"> Explore Meals </Link>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+}
