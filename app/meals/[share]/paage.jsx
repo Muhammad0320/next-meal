d0ed52f50722a4fca9445e50e9e4bab2b5926cb5
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-01 12:01:50 +0100

    made the share route dynamic

---
+++ app/meals/[share]/paage.jsx
@@ -0,0 +1,9 @@
+function MealShare({ params }) {
+  return (
+    <main>
+      <h1> Meal {params.share} page </h1>
+    </main>
+  );
+}
+
+export default MealShare;
--- app/meals/share/paage.jsx
+++
@@ -1,9 +0,0 @@
-function MealShare() {
-  return (
-    <main>
-      <h1> Meal share page </h1>
-    </main>
-  );
-}
-
-export default MealShare;
