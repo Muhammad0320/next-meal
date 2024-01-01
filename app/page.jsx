a86ea86f19a3983aa1fec7849e8ee8cff1267ae3
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-01 22:35:16 +0100

    chore: add styled and text to page comp

--- app/page.jsx
+++ app/page.jsx
@@ -8,15 +8,45 @@ export default function Home() {
       <header className={classes.header}>
         <div className={classes.slideshow}></div>
 
-        <div className={classes.hero}>
-          <h1> NextLevel Foods for NextLevel Foodies </h1>
-          <p>Tast and share food from all over the world</p>
-        </div>
-        <div className={classes.cta}>
-          <Link href="/community"> Join the community </Link>
-          <Link href="meals"> Explore Meals </Link>
+        <div>
+          <div className={classes.hero}>
+            <h1> NextLevel Foods for NextLevel Foodies </h1>
+            <p>Tast and share food from all over the world</p>
+          </div>
+
+          <div className={classes.cta}>
+            <Link href="/community"> Join the community </Link>
+            <Link href="/meals"> Explore Meals </Link>
+          </div>
         </div>
       </header>
+      <main>
+        <section className={classes.section}>
+          <h2>How it works</h2>
+          <p>
+            NextLevel Food is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes,
+            and to connect with other food lovers.
+          </p>
+          <p>
+            NextLevel Food is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
+
+        <section className={classes.section}>
+          <h2>Why NextLevel Food?</h2>
+          <p>
+            NextLevel Food is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes,
+            and to connect with other food lovers.
+          </p>
+          <p>
+            NextLevel Food is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
+      </main>
     </>
   );
 }
