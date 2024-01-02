6766bdd293653bbc98179c68ba35c1ff22ea95be
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-02 16:09:00 +0100

    chore: update error comp

--- app/meals/error.jsx
+++ app/meals/error.jsx
@@ -5,7 +5,7 @@ function ErrorPage() {
     <main className="error">
       <h1> An error occured </h1>
 
-      <p> Failed to fetch meals data </p>
+      <p> Failed to fetch meals data. Please try again later</p>
     </main>
   );
 }
