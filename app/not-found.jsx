e19b5ae2a43995edf195a0abd0f0e5614f14c9c0
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-02 16:10:56 +0100

    chore: fixed type in text

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -3,7 +3,7 @@ function NotFound() {
     <main className="not-found">
       <h1> Not Found </h1>
 
-      <p> Unfortunately, we coulf not find requested page or resource </p>
+      <p> Unfortunately, we could not find requested page or resource </p>
     </main>
   );
 }
