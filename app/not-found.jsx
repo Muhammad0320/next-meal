19dd237e188dfd93792856a34061436dfb13dcab
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-02 16:08:16 +0100

    chore: add not found compo

---
+++ app/not-found.jsx
@@ -0,0 +1,11 @@
+function NotFound() {
+  return (
+    <main className="not-found">
+      <h1> Not Found </h1>
+
+      <p> Unfortunately, we coulf not find requested page or resource </p>
+    </main>
+  );
+}
+
+export default NotFound;
