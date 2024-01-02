e49d4e491c430194a722712ba611a11442947acc
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-02 08:02:32 +0100

    chore: update meals page comp

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,13 +1,27 @@
 import Link from "next/link";
 
+import classes from "./page.module.css";
+
 function Meals() {
   return (
-    <main>
-      <h1> The meals page </h1>
-      <p>
-        <Link href={"/meals/share"}> Meal share </Link>
-      </p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals created{" "}
+          <span className={classes.highlidht}> bu you </span>
+        </h1>
+
+        <p>
+          choose your favorite recipe and cook it yourself. It is easy ans fun!
+        </p>
+
+        <p className={classes.cta}>
+          <Link href="/meals/share"> Share your Favourite recipe </Link>
+        </p>
+      </header>
+
+      <main className={classes.main}></main>
+    </>
   );
 }
 
