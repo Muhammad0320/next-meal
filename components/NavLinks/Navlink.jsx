12f034502530abd94e8c9cafaedf97695b19351f
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-02 07:44:08 +0100

    chroe:  update navlink user comp

--- components/NavLinks/Navlink.jsx
+++ components/NavLinks/Navlink.jsx
@@ -1,9 +1,25 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import classes from "./NavLink.module.css";
 
 function Navlink({ children, href }) {
-  return <Link></Link>;
+  const path = usePathname();
+
+  return (
+    <Link
+      href={href}
+      className={
+        path.startsWith(href)
+          ? `${classes.link}  ${classes.active}`
+          : `${classes.link}`
+      }
+    >
+      {children}
+    </Link>
+  );
 }
 
 export default Navlink;
