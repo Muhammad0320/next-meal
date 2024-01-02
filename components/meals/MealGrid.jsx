8e48d72d16cf6c80c44067b982972e822071622c
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-02 08:25:56 +0100

    chroe: update mealgrid comp

--- components/meals/MealGrid.jsx
+++ components/meals/MealGrid.jsx
@@ -1,10 +1,13 @@
+import MealItem from "./MealItem";
 import classes from "./MealsGrid.module.css";
 
 function MealGrid({ meals }) {
   return (
     <ul className={classes.meals}>
       {meals.map((meal) => (
-        <li key={meal.id}></li>
+        <li key={meal.id}>
+          <MealItem {...meal} />
+        </li>
       ))}
     </ul>
   );
