e0c8ad6ea3983da86e5c9f9f618cb71d61129f1e
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-03 16:48:01 +0100

    chore: update image picker comp

--- app/meals/share/ImagePicker.jsx
+++ app/meals/share/ImagePicker.jsx
@@ -1,5 +1,20 @@
-function ImagePicker() {
-  return <div></div>;
+import classes from "./ImagePicker.module.css";
+
+function ImagePicker({ label, name }) {
+  return (
+    <div className={classes.picker}>
+      <label htmlFor={name}> {label} </label>
+
+      <div className={classes.controls}>
+        <input
+          type="file"
+          accept="image/png, image/jpeg"
+          name={name}
+          id={name}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default ImagePicker;
